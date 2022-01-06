425991150d9792766eb50eebb6800cafa8204202
Kheradiya <97148075+Kheradiya@users.noreply.github.com>
2022-01-06 14:57:41 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.hello;
+
+public class Hello 
+{
+	private String message;
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+	public void setMessage(String message)
+	{
+		this.setMessage(message);
+	}
+	public void getMessage() {
+		System.out.println("Message : "+ message );
+	}
+}
+ 
