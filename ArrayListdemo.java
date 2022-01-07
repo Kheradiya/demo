dabe44cff90e48ef42de1457b169d8f020b8c3b4
Kheradiya <97148075+Kheradiya@users.noreply.github.com>
2022-01-07 13:37:51 +0530

    Add files via upload

---
+++ ArrayListdemo.java
@@ -0,0 +1,17 @@
+
+package com.ArrayList;
+import java.util.*;
+
+public class ArrayListdemo 
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("arraylist");
+		list.add("hashset");
+		list.add("linkedlist");
+		list.add("hashmap");
+		
+		System.out.println(list);
+	}	
+}
