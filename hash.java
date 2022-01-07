256bda2cbbc9a81ac9573fff08f218109f1b5d28
Kheradiya <97148075+Kheradiya@users.noreply.github.com>
2022-01-07 17:30:19 +0530

    Add files via upload

---
+++ hash.java
@@ -0,0 +1,19 @@
+package hashset;
+
+import java.util.*;  
+class HashSet1{  
+ public static void main(String args[]){  
+ 
+  HashSet<String> set=new HashSet<String>();  
+  set.add("7");  
+  set.add("1");  
+  set.add("6");  
+  set.add("3");  
+  set.add("9");  
+  set.add("4");  
+  set.add("0");  
+
+ 
+   System.out.println(set);  
+  }  
+ }  
